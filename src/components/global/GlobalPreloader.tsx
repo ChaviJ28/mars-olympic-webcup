c83510f932f939ca64afd1e5548de7f9f263d7fc
Chavi Surujbhali <surujbhalichavi@gmail.com>
2024-05-04 17:41:09 +0400

    fEAT: PRELOADER

--- src/components/global/GlobalPreloader.tsx
+++ src/components/global/GlobalPreloader.tsx
@@ -87,7 +87,7 @@ export function GlobalPreloader({ onAnimComplete }: { onAnimComplete: Dispatch<S
     };
   
     return (
-      <section ref={root} className={clsx("fixed top-0 z-[110] size-full bg-linearGradient", styles.globalPreloader)}>
+      <section ref={root} className={clsx("fixed top-0 z-[110] size-full bg-[url('/images/preloader/space.webp')]", styles.globalPreloader)}>
         <div ref={spinner} className="absolute top-0 z-20 grid size-full place-items-center bg-[url('/images/preloader/space.webp')]">
           <div className="relative z-20 flex flex-wrap justify-center">
             <div className="relative">
@@ -115,76 +115,71 @@ export function GlobalPreloader({ onAnimComplete }: { onAnimComplete: Dispatch<S
           <img className="mx-auto" src="/images/logo.svg" alt="Lingo logo" width="118" height="32" />
   
           <div className="mx-auto flex w-full max-w-[1140px] flex-wrap justify-between px-24 xxl:max-w-[900px] l:max-w-[500px]">
-            <div className="mb-56 flex w-full flex-wrap justify-between l:mb-32 s:grid s:grid-cols-2 s:gap-16">
-              <div>
-                <p className="mb-8 font-lores uppercase tracking-tighter text-black">Gate</p>
-                <div className="flex gap-4 xxl:gap-2 s:grid s:grid-cols-4">
-                  <BaseFlipper className="s:w-auto" play={playFlipper} offset={0.1}>
-                    A
-                  </BaseFlipper>
-                  <BaseFlipper className="s:w-auto" play={playFlipper} offset={0.2}>
-                    T
-                  </BaseFlipper>
-                  <BaseFlipper className="s:w-auto" play={playFlipper} offset={0.3}>
-                    H
-                  </BaseFlipper>
-                </div>
-              </div>
-              <div>
-                <p className="mb-8 font-lores uppercase tracking-tighter text-black">Flight No.</p>
-                <div className="flex gap-4 xxl:gap-2 s:grid s:grid-cols-5">
-                  <BaseFlipper className="s:w-auto" play={playFlipper} offset={0.8}>
-                    L
-                  </BaseFlipper>
-                  <BaseFlipper className="s:w-auto" play={playFlipper} offset={0.9}>
-                    F
-                  </BaseFlipper>
-                  <BaseFlipper className="s:w-auto" play={playFlipper} offset={1.0}>
-                    G
-                  </BaseFlipper>
-                  <BaseFlipper className="s:w-auto" play={playFlipper} offset={1.1}>
-                    2
-                  </BaseFlipper>
-                  <BaseFlipper className="s:w-auto" play={playFlipper} offset={1.2}>
-                    4
-                  </BaseFlipper>
-                </div>
-              </div>
-              <div className="l:mt-24 l:w-full s:col-span-2 s:mt-0">
-                <div className="relative flex gap-4">
-                  <p className="mb-8 font-lores uppercase tracking-tighter text-black">Destination</p>
-                </div>
-                <div className="flex gap-4 xxl:gap-2">
-                  <BaseFlipper className="l:!w-1/5" play={playFlipper} offset={1.7}>
-                    L
-                  </BaseFlipper>
-                  <BaseFlipper className="l:!w-1/5" play={playFlipper} offset={1.8}>
-                    I
-                  </BaseFlipper>
-                  <BaseFlipper className="l:!w-1/5" play={playFlipper} offset={1.9}>
-                    N
-                  </BaseFlipper>
-                  <BaseFlipper className="l:!w-1/5" play={playFlipper} offset={2.0}>
-                    G
-                  </BaseFlipper>
-                  <BaseFlipper className="l:!w-1/5" play={playFlipper} offset={2.1}>
-                    0
-                  </BaseFlipper>
-                </div>
+          <div className="mb-56 flex w-full flex-wrap justify-between l:mb-32 s:grid s:grid-cols-2 s:gap-16">
+            <div>
+              <div className="flex gap-4 xxl:gap-2 s:grid s:grid-cols-4">
+                <BaseFlipper className="s:w-auto" play={playFlipper} offset={0.1}>
+                  W
+                </BaseFlipper>
+                <BaseFlipper className="s:w-auto" play={playFlipper} offset={0.2}>
+                 E
+                </BaseFlipper>
+                <BaseFlipper className="s:w-auto" play={playFlipper} offset={0.3}>
+                  L
+                </BaseFlipper>
+                <BaseFlipper className="s:w-auto" play={playFlipper} offset={0.1}>
+                  C
+                </BaseFlipper>
+                <BaseFlipper className="s:w-auto" play={playFlipper} offset={0.2}>
+                  O
+                </BaseFlipper>
+                <BaseFlipper className="s:w-auto" play={playFlipper} offset={0.3}>
+                  M
+                </BaseFlipper>
+                <BaseFlipper className="s:w-auto" play={playFlipper} offset={0.3}>
+                  E
+                </BaseFlipper>
               </div>
             </div>
-  
-            <div className="w-full">
-              <BaseBtn className="mx-auto s:w-full" onClick={mapTransition} size="large" variant="glow">
-                FLY TO AIRDROP ISLANDS
-                <BaseIcon name="arrow-right-fill" className="ml-8" width="32px" height="32px" />
-              </BaseBtn>
-  
-              <p className="mt-16 hidden text-center text-14 l:block xxs:text-12">
-                *for the best experience, please view on a larger display
-              </p>
+            <div>
+              <div className="flex gap-4 xxl:gap-2 s:grid s:grid-cols-5">
+                <BaseFlipper className="s:w-auto" play={playFlipper} offset={0.8}>
+                 T
+                </BaseFlipper>
+                <BaseFlipper className="s:w-auto" play={playFlipper} offset={0.9}>
+                 O
+                </BaseFlipper>
+              </div>
+            </div>
+            <div className="l:mt-24 l:w-full s:col-span-2 s:mt-0">
+              <div className="flex gap-4 xxl:gap-2">
+                <BaseFlipper className="l:!w-1/4" play={playFlipper} offset={1.7}>
+                  M
+                </BaseFlipper>
+                <BaseFlipper className="l:!w-1/4" play={playFlipper} offset={1.8}>
+                  A
+                </BaseFlipper>
+                <BaseFlipper className="l:!w-1/4" play={playFlipper} offset={1.9}>
+                  R
+                </BaseFlipper>
+                <BaseFlipper className="l:!w-1/4" play={playFlipper} offset={2.0}>
+                  S
+                </BaseFlipper>
+              </div>
             </div>
           </div>
+
+          <div className="w-full">
+            <BaseBtn className="mx-auto s:w-full" onClick={mapTransition} size="large" variant="glow">
+              Fly To Olympics 2092
+              <BaseIcon name="arrow-right-fill" className="ml-8" width="32px" height="32px" />
+            </BaseBtn>
+
+            <p className="mt-16 hidden text-center text-14 l:block xxs:text-12">
+              *for the best experience, please view on a larger display
+            </p>
+          </div>
+        </div>
         </div>
   
         <canvas ref={canvas} className="absolute inset-0 z-10" />
