e7046123b9eb81a4c7145588293692b666fc8239
Chavi Surujbhali <surujbhalichavi@gmail.com>
2024-05-04 16:57:59 +0400

    Feat: project setup

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import { AnimatePresence } from "framer-motion";
+import './App.css';
+import { Route, Routes, useLocation } from "react-router-dom";
+import Homepage from "./pages/home";
+
+function App() {
+  const location = useLocation();
+
+  return (
+    <main className="font-sans">
+      <AnimatePresence>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </AnimatePresence>
+      {/* <FPSStats /> */}
+    </main>
+  );
+}
+
+export default App;
